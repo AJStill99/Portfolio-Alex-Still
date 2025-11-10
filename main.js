95f67950571239ccbf29caad9c9abe930b5e446b
Alex Still <alexstill@hotmail.co.uk>
2025-11-10 01:10:40 +0000

    Adding JavaScript functionality for first two buttons - deciding what to do for the 3rd

--- main.js
+++ main.js
@@ -8,8 +8,25 @@ function onButtonClick() {
 }
 
 function handleClick() {
-    alert("Button Clicked!");
+    alert("Button Clicked! (Testing Playwright interaction...)");
+    // Note this alert will also show the IP address followed by 'says' when run in chrome
+    console.log("Alert displayed for button click.");
+    console.log(`Button clicked at ${new Date().toLocaleTimeString()}`);
+    // This should give the time the button was clicked
 }
+
+function changeBackgroundColorToggle() {
+    const body = document.body;
+    const currentColor = body.style.backgroundColor;
+    if(currentColor === 'blue') {
+        body.style.backgroundColor = 'white';
+        console.log("Background color changed to white.");
+    } else {
+        body.style.backgroundColor = 'blue';
+        console.log("Background color changed to lightblue.");
+    }
+}
+// Simple toggle function to change background color on button click
 
 /* INPUT FIELDS
     Functions that will be attached to input fields
