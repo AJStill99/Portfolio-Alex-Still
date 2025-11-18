5c7ea62cffa1f96fcde0a75e47dffef2e99da195
Alex Still <alexstill@hotmail.co.uk>
2025-11-18 00:15:47 +0000

    Comments

--- tests/SD_LP.spec.js
+++ tests/SD_LP.spec.js
@@ -7,7 +7,7 @@ test.describe('Running tests on the Sauce Demo landing page - showcasing some ba
     test('Go to landing page', async ({ page }) => {
         await page.goto('./'); // Have set the base URL in the config file to be sauce demo
         await expect(page.title()).toBe('Sauce Demo');
-    })
+    }) // Opted to do this manually rather than my helper function to showcase the base URL
 
     test
 })
