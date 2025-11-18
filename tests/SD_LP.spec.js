5cc71e4aa3101e6eb684c18e351018227fd8890f
Alex Still <alexstill@hotmail.co.uk>
2025-11-18 00:14:21 +0000

    Removed example file, and added a test for Sauce Demo tests

---
+++ tests/SD_LP.spec.js
@@ -0,0 +1,13 @@
+// Sauce Demo Landing Page
+
+const { test } = require('../fixtures'); // import custom fixtures (if needed)
+const { expect } = require('@playwright/test');
+
+test.describe('Running tests on the Sauce Demo landing page - showcasing some basic UI automation, and the use of baseURL', () => {
+    test('Go to landing page', async ({ page }) => {
+        await page.goto('./'); // Have set the base URL in the config file to be sauce demo
+        await expect(page.title()).toBe('Sauce Demo');
+    })
+
+    test
+})
